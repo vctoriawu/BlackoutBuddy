c3b120645806dd1603c7a7db3e88a9b7bf369e0b
JD <johndavid.anthony1@gmail.com>
2020-02-01 12:37:14 -0500

    Added Eureka

--- iPhone/Blackout Buddy/Controller/SecondViewController.swift
+++ iPhone/Blackout Buddy/Controller/SecondViewController.swift
@@ -7,14 +7,25 @@
 //
 
 import UIKit
+import Eureka
 
-class SecondViewController: UIViewController {
+class SecondViewController: FormViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        form +++ Section("Section1")
+            <<< TextRow(){ row in
+                row.title = "Text Row"
+                row.placeholder = "Enter text here"
+            }
+            <<< PhoneRow(){
+                $0.title = "Phone Row"
+                $0.placeholder = "And numbers here"
+            }
+        +++ Section("Section2")
+            <<< DateRow(){
+                $0.title = "Date Row"
+                $0.value = Date(timeIntervalSinceReferenceDate: 0)
+            }
     }
-
-
 }
-
---
+++ iPhone/Pods/Target Support Files/Pods-Blackout Buddy-Blackout BuddyUITests/Pods-Blackout Buddy-Blackout BuddyUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Blackout_Buddy_Blackout_BuddyUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Blackout_Buddy_Blackout_BuddyUITests
+@end
