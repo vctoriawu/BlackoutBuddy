13cdf43a86c4ac480148da3dd8f5132d8013871e
victoriaaarmstrong <57503137+victoriaaarmstrong@users.noreply.github.com>
2020-02-01 11:24:58 -0500

    Updated reminders functionality

--- iPhone/Blackout Buddy/Controller/SecondViewController.swift
+++ iPhone/Blackout Buddy/Controller/SecondViewController.swift
@@ -8,8 +8,34 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var dataList = ["1", "2", "3"]
+    @IBOutlet weak var myTableView: UITableView!
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return (dataList.count)
+    }
 
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = UITableViewCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: "cell1")
+        cell.textLabel?.text = dataList[indexPath.row]
+        
+        return(cell)
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath)
+    {
+        if editingStyle == UITableViewCell.EditingStyle.delete
+        {
+            self.dataList.remove(at: indexPath.row)
+            myTableView.reloadData()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
