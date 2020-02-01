13d6b86087fd427adf6b4c560a29ed03e6f5781e
stefanoroque <stefanoroque98@gmail.com>
2020-02-01 15:36:59 -0500

    added map and centre to current loaction

--- iPhone/Blackout Buddy/Controller/ThirdViewController.swift
+++ iPhone/Blackout Buddy/Controller/ThirdViewController.swift
@@ -7,14 +7,68 @@
 //
 
 import UIKit
+import MapKit
 
 class ThirdViewController: UIViewController {
+    @IBOutlet weak var mapView: MKMapView!
+    
+    //fileprivate let locationManager:CLLocationManager = CLLocationManager()
+    fileprivate let locationManager: CLLocationManager = {
+       let manager = CLLocationManager()
+       manager.requestWhenInUseAuthorization()
+       return manager
+    }()
+    
+    var stages = CLLocationCoordinate2DMake(44.2329299, -76.4942019)
+    var home = CLLocationCoordinate2DMake(44.2363074, -76.4999346)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        //for branching
-        //sjnjnsjdnsjndjs
+        //updateMapRegion(place: home, rangeSpan: 500)
+        
+        setUpMapView()
+        
     }
+    
+    
+    func setUpMapView() {
+       mapView.showsUserLocation = true
+       mapView.showsCompass = true
+       mapView.showsScale = true
+       currentLocation()
+    }
+    
+    func updateMapRegion(place:CLLocationCoordinate2D , rangeSpan:CLLocationDistance) {
+        let region = MKCoordinateRegion.init(center: place, latitudinalMeters: rangeSpan, longitudinalMeters: rangeSpan)
+        mapView.region = region
+    }
+    
+    func currentLocation() {
+       locationManager.delegate = self
+       locationManager.desiredAccuracy = kCLLocationAccuracyBest
+       if #available(iOS 11.0, *) {
+          locationManager.showsBackgroundLocationIndicator = true
+       } else {
+          // Fallback on earlier versions
+       }
+       locationManager.startUpdatingLocation()
+    }
+    
+    
+}
+
+extension ThirdViewController: CLLocationManagerDelegate {
+     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations.last! as CLLocation
+        let currentLocation = location.coordinate
+        let coordinateRegion = MKCoordinateRegion(center: currentLocation, latitudinalMeters: 800, longitudinalMeters: 800)
+        mapView.setRegion(coordinateRegion, animated: true)
+        locationManager.stopUpdatingLocation()
+     }
+     
+     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error.localizedDescription)
+     }
     
     
 }
